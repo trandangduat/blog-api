dac36b29b882ac5c6fc210c1d457c7d672565d9f
Tran Dang Duat <23020023@vnu.edu.vn>
2024-07-05 14:06:35 +0700

    Init express app

---
+++ backend/app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+
+const indexRouter = require("./routes/index");
+const app = express();
+
+app.use('/', indexRouter);
+
+app.listen(3000, () => console.log("Listening on port 3000!"));
