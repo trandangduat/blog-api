5d0b14117253c89956004de1ebf459bf22683a63
Tran Dang Duat <23020023@vnu.edu.vn>
2024-07-07 22:01:23 +0700

    Add CORS & Test fetching data from server and displaying on client page

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 require('dotenv').config();
 
 main().catch(err => console.log(err));
@@ -11,6 +12,7 @@ async function main() {
 const indexRouter = require("./routes/index");
 const app = express();
 
+app.use(cors());
 app.use('/', indexRouter);
 
 app.listen(3000, () => console.log("Listening on port 3000!"));
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,27 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetch('http://localhost:3000/api/posts')
+      .then(res => res.json())
+      .then(res => setData(res.all_posts));
+  }, []);
 
   return (
     <>
-    <h1>Hello world</h1>    
+      <h1>Hello world</h1>
+      {data && data.map((post) => {
+        return (
+          <>
+            <div key = {post._id}>
+              <p>{post.title}</p>
+              <p>{post.body}</p>
+            </div>
+          </>
+        );
+      })}
     </>
   )
 }
