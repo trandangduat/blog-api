cc0fd8721c1d428e850f6627c8a95471b2f48169
Tran Dang Duat <23020023@vnu.edu.vn>
2024-07-25 17:55:22 +0700

    Add Drawer animation (slide up)

--- frontend/src/components/Drawer.jsx
+++ frontend/src/components/Drawer.jsx
@@ -1,6 +1,39 @@
-import { useState } from "react";
+import { useSpring, animated } from "@react-spring/web"
+import { useEffect } from "react";
 
 export const Drawer = ({ children, isVisible, setIsVisible }) => {
+    const [drawerAnimation, drawer] = useSpring(() => ({
+        from: { bottom: "-100%" },
+    }));
+    const [backdropAnimation, backdrop] = useSpring(() => ({
+        from: { opacity: "0" },
+    }));
+
+    useEffect(() => {
+        if (isVisible) {
+            backdrop.start({
+                from: { opacity: "0" },
+                to: { opacity: "1" },
+            });
+            drawer.start({
+                from: { bottom: "-100%" },
+                to: { bottom: "0" },
+            });
+        }
+    }, [isVisible, drawer, backdrop]);
+
+    const closeDrawer = () => {
+        drawer.start({
+            from: { bottom: "0" },
+            to: { bottom: "-100%" },
+        });
+        backdrop.start({
+            from: { opacity: "1" },
+            to: { opacity: "0" },
+        });
+        setTimeout(() => setIsVisible(false), 300);
+    };
+
     return (
         <div 
             className={`
@@ -8,22 +41,28 @@ export const Drawer = ({ children, isVisible, setIsVisible }) => {
                 ${isVisible ? "visible" : "hidden"}
             `}
         >
-            <div
+            <animated.div
                 className={`
                     fixed top-0 left-0 w-screen h-screen
                     bg-slate-900/50 backdrop-blur-sm
                 `}
-                onClick={() => setIsVisible(false)}
+                onClick={closeDrawer}
+                style={{
+                    ...backdropAnimation,
+                }}
             >
-            </div>
-            <div 
+            </animated.div>
+            <animated.div 
                 className={`
                     bg-white rounded-t-xl overflow-auto 
-                    mx-auto max-w-4xl h-5/6 fixed left-0 bottom-0 right-0
+                    mx-auto max-w-4xl h-5/6 fixed left-0 right-0
                 `}
+                style={{
+                    ...drawerAnimation,
+                }}
             >
                 {children}
-            </div>
+            </animated.div>
         </div>
     );
 }
